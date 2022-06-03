032d3c95778e72da02e22425812a9b20043fcf3f
Wasiu-hub <contactbow@yahoo.com>
2022-06-03 01:02:04 +0100

    update8

--- src/model/Room.java
+++ src/model/Room.java
@@ -6,87 +6,67 @@ import java.util.Set;
 /**
  * Class Room - a room in an adventure game.
  *
- * This class is part of the "World of Zuul" application. 
- * "World of Zuul" is a very simple, text based adventure game.  
+ * This class is part of the "World of Zuul" application. "World of Zuul" is a
+ * very simple, text based adventure game.
  *
- * A "Room" represents one location in the scenery of the game.  It is 
- * connected to other rooms via exits.  The exits are labelled north, 
- * east, south, west.  For each direction, the room stores a reference
- * to the neighboring room, or null if there is no exit in that direction.
+ * A "Room" represents one location in the scenery of the game. It is connected
+ * to other rooms via exits. The exits are labelled north, east, south, west.
+ * For each direction, the room stores a reference to the neighboring room, or
+ * null if there is no exit in that direction.
  * 
- * @author  Michael Kolling and David J. Barnes
+ * @author Michael Kolling and David J. Barnes
  * @version 2011.07.31
  */
-public class Room 
-{
-    private String description;
+public class Room {
+	private String description;
 //    private Room northExit;
 //    private Room southExit;
 //    private Room eastExit;
 //    private Room westExit;
-    private HashMap<String, Room> exits;
+	private HashMap<String, Room> exits;
 
-    /**
-     * Create a room described "description". Initially, it has
-     * no exits. "description" is something like "a kitchen" or
-     * "an open court yard".
-     * @param description The room's description.
-     */
-    public Room(String description) 
-    {
-        this.description = description;
-    }
+	/**
+	 * Create a room described "description". Initially, it has no exits.
+	 * "description" is something like "a kitchen" or "an open court yard".
+	 * 
+	 * @param description The room's description.
+	 */
+	public Room(String description) {
+		this.description = description;
+	}
 
-    /**
-     * Define the exits of this room.  Every direction either leads
-     * to another room or is null (no exit there).
-     * @param north The north exit.
-     * @param east The east east.
-     * @param south The south exit.
-     * @param west The west exit.
-     */
-    public void setExits(String direction, Room destination) 
-    {
-        exits.put(direction, destination);
-    }
+	/**
+	 * Define the exits of this room. Every direction either leads to another room
+	 * or is null (no exit there).
+	 * 
+	 * @param north The north exit.
+	 * @param east  The east east.
+	 * @param south The south exit.
+	 * @param west  The west exit.
+	 */
+	public void setExits(String direction, Room destination) {
+		exits.put(direction, destination);
+	}
 
-    /**
-     * @return The description of the room.
-     */
-    public String getDescription() {
-        return description;
-    }
-    
-    public String getExits() {
-    	String exitString = "";
-    	Set<String> exitDirections = exits.keySet(); // all directions 
-    	for(String dir : exitDirections) {
-    		exitString = exitString + dir + " ";
-    	}
-    	
-		return exitString;    
+	/**
+	 * @return The description of the room.
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	public String getExits() {
+		String exitString = "";
+		Set<String> exitDirections = exits.keySet(); // all directions
+		for (String dir : exitDirections) {
+			exitString = exitString + dir + " ";
+		}
+
+		return exitString;
 	}
 
 	public Room getExit(String direction) {
-		if(direction.equals("north")) {
-            return northExit;
-        }
-        if(direction.equals("east")) {
-            return eastExit;
-        }
-        if(direction.equals("south")) {
-            return southExit;
-        }
-        if(direction.equals("west")) {
-            return westExit;
-        }
-        return null;
+		return exits.get(direction);
 	}
 
-	
-
-
-
-
 }
-
